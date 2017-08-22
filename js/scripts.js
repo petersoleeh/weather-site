239299fc1d998ce619a9abd2011706d475761959
peter soleeh <petersoleeh@gmail.com>
2017-08-22 16:46:37 +0300

    links to cites activating links to the different cities

--- js/scripts.js
+++ js/scripts.js
@@ -22,74 +22,80 @@ var weatherDescription = new String(weather_data.weather[0].description)
 }
 
 //frontend
-$.get({
-  url: 'http://api.openweathermap.org/data/2.5/weather?q=london&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
-  success: function(weather_data){
-    to_be_run_on_server_response(weather_data)
-  }
 
-});
+$(document).ready(function() {
+
+  $('#city1').click(function(event) {
+    event.preventDefault();
+    $('.clearField').empty();
+    alert("The data for Nairobi is currently being fetched");
+    $.get({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Nairobi&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
+      success: function(weather_data){
+        to_be_run_on_server_response(weather_data);
+      }
+    })
+  })
 
 
 
- // $(document).ready(function() {
- //   $('#city1').click(function(event)) {
- //     event.preventDefault();
- //     $('.clearField').empty();
- //
- //     $.get({
- //       url: 'http://api.openweathermap.org/data/2.5/weather?q=Daressalaam&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
- //       success: function(weather_data){
- //         to_be_run_on_server_response(weather_data)
- //       }
- //     }
- //   }
- // });
- //
- // $(document).ready(function() {
- //    $ ('#city2').click(function(event)) {
- //    event.preventDefault();
- //    $('.clearField').empty();
- //
- //      $.get({
- //        url: 'http://api.openweathermap.org/data/2.5/weather?q=Kampala&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
- //         success: function(weather_data){
- //          to_be_run_on_server_response(weather_data)
- //     }
- //   });
- //
- //   $(document).ready(function() {
- //    $ ('#city3').click(function(event)) {
- //      event.preventDefault();
- //       $('.clearField').empty();
- //
- //        $.get({
- //           url: 'http://api.openweathermap.org/data/2.5/weather?q=Kigali&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
- //           success: function(weather_data){
- //             to_be_run_on_server_response(weather_data)
- //      }
- //     });
- //
- //   $(document).ready(function() {
- //    $ ('#city4').click(function(event)) {
- //       event.preventDefault();
- //      $('.clearField').empty();
- //
- //         $.get({
- //           url: 'http://api.openweathermap.org/data/2.5/weather?q=Johannesburg&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
- //           success: function(weather_data){
- //             to_be_run_on_server_response(weather_data)
- //      }
- //    });
- //
- //     $(document).ready(function() {
- //       $ ('#city5').click(function(event)) {
- //        event.preventDefault();
- //        $('.clearField').empty();
- //
- //          $.get({
- //            url: 'http://api.openweathermap.org/data/2.5/weather?q=AddisAbaba&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
- //            success: function(weather_data){
- //              to_be_run_on_server_response(weather_data)
- //         }
- //      });
+$('#city2').click(function(event) {
+  event.preventDefault();
+  $('.clearField').empty();
+  alert("The data for Dar es salaam is currently being fetched");
+  $.get({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=Daressalaam&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
+    success: function(weather_data){
+      to_be_run_on_server_response(weather_data);
+    }
+   })
+  })
+
+  $('#city3').click(function(event) {
+    event.preventDefault();
+    $('.clearField').empty();
+    alert("The data for Kampala is currently being fetched");
+    $.get({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=kampala&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
+      success: function(weather_data){
+        to_be_run_on_server_response(weather_data);
+      }
+     })
+    })
+    $('#city4').click(function(event) {
+      event.preventDefault();
+      $('.clearField').empty();
+      alert("The data for Kigali is currently being fetched");
+      $.get({
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=kigali&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
+        success: function(weather_data){
+          to_be_run_on_server_response(weather_data);
+        }
+       })
+      })
+
+
+      $('#city5').click(function(event) {
+        event.preventDefault();
+        $('.clearField').empty();
+        alert("The data for Johanesburg is currently being fetched");
+        $.get({
+          url: 'http://api.openweathermap.org/data/2.5/weather?q=johanesburg&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
+          success: function(weather_data){
+            to_be_run_on_server_response(weather_data);
+          }
+         })
+        })
+
+        $('#city6').click(function(event) {
+          event.preventDefault();
+          $('.clearField').empty();
+          alert("The data for Addis Ababa is currently being fetched");
+          $.get({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=addisababa&mode=json&units=imperial&APPID=248a7533aec9546a0b5f48951b31e33a',
+            success: function(weather_data){
+              to_be_run_on_server_response(weather_data);
+            }
+           })
+          })
+        });
